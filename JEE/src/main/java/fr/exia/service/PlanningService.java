eaa0091cf3167ef05e1e487a9dff9527c1418818
Florent Bethenod <florent.bethenod@viacesi.fr>
2015-06-17 09:41:34 +0200

    adding planningOpti & time modif

--- JEE/src/main/java/fr/exia/service/PlanningService.java
+++ JEE/src/main/java/fr/exia/service/PlanningService.java
@@ -30,7 +30,6 @@ public class PlanningService {
 	private HoraireRepository hrepos;
 	@Inject
 	private ActiviteRepository arepos;
-
 	@Inject
 	private NoteRepository nrepos;
 
@@ -38,13 +37,15 @@ public class PlanningService {
 		String[] parse = plage.split(";|\\:");
 		LocalDate debut = new LocalDate(parse[0]);
 		LocalDate fin = new LocalDate(parse[2]);
-		log.debug("Recherche des activités sur la plage: "+debut.toString()+" - "+parse[1]+ " - " + fin.toString() +"-"+ parse[3]);
+		log.debug("Recherche des activités sur la plage: " + debut.toString()
+				+ " - " + parse[1] + " - " + fin.toString() + "-" + parse[3]);
 		List<Activite> lista = new ArrayList<Activite>();
 		for (Horaire h : hrepos.findAll()) {
-			if ((h.getDate_debut().isAfter(debut)||h.getDate_debut().isEqual(debut))
+			if ((h.getDate_debut().isAfter(debut) || h.getDate_debut().isEqual(
+					debut))
 					&& h.getDate_debut().isBefore(fin)
-					&& (h.getHeure_debut()>=Integer.parseInt(parse[1]))
-					&& h.getHeure_debut()<Integer.parseInt(parse[3])) {
+					&& (h.getHeure_debut() >= Integer.parseInt(parse[1]))
+					&& h.getHeure_debut() < Integer.parseInt(parse[3])) {
 				for (Activite a : h.getActivites()) {
 					lista.add(a);
 				}
@@ -53,8 +54,48 @@ public class PlanningService {
 		return lista;
 	}
 
-	public List<Activite> planingOpti() { // en cours
+	public List<Activite> planingOpti(int hrequest) { // valid� reste tsortie
 		List<Activite> lista = new ArrayList<Activite>();
+		int HeureFinPrec = hrequest, HeureDebutSuiv = 0;
+		int deplacement = 10, dureeRepas = 0100;
+		Activite prochaineActivite = null;
+		boolean repasMidi=false,repasSoir=false;
+
+		while (HeureFinPrec < 2200) {
+
+			int tempsAttente = 10000;
+			for (Activite a : arepos.findAll()) {
+				for (Horaire h : a.getHoraires()) {
+					HeureDebutSuiv = h.getHeure_debut();
+					int tempsAttenteTempx = HeureDebutSuiv - HeureFinPrec;
+					if (tempsAttenteTempx < tempsAttente
+							&& tempsAttenteTempx > 0) {
+						tempsAttente = tempsAttenteTempx;
+						prochaineActivite = a;
+					}
+				}
+			}
+			lista.add(prochaineActivite);
+			HeureFinPrec =formatage( HeureDebutSuiv
+					+ prochaineActivite.getDuree_activite() + deplacement);
+			if (HeureFinPrec >= 1200 && HeureFinPrec < 1400 && repasMidi==false) {
+				repasMidi=true;
+				HeureFinPrec+=dureeRepas;
+			}
+			if (HeureFinPrec >= 1930 && HeureFinPrec < 2200 && repasSoir==false) {
+				repasSoir=true;
+				HeureFinPrec+=dureeRepas;
+			}
+		}
 		return lista;
+	}
+
+	private int formatage(int i) {
+		int x = (i/100)*100;
+		int y = i - x;
+		int z = y/60;
+		int r = z*60;
+		i = x + z + r;
+		return i;
 	}
 }
