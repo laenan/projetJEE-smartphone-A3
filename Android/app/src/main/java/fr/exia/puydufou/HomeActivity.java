6678ccf6bdb6c1b347496e2e357a8a2b7affdd2c
Alexandre Raguin <Alexandre.Raguin@gmail.com>
2015-06-17 14:50:14 +0200

    WIP deleteing homefragment

--- Android/app/src/main/java/fr/exia/puydufou/HomeActivity.java
+++ Android/app/src/main/java/fr/exia/puydufou/HomeActivity.java
@@ -17,10 +17,7 @@ public class HomeActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+
 	}
 
 	@Override
@@ -42,10 +39,13 @@ public class HomeActivity extends ActionBarActivity {
 		return super.onOptionsItemSelected(item);
 	}
 
+
+
+
 	/**
 	 * A placeholder fragment containing a simple view.
 	 */
-	public static class PlaceholderFragment extends Fragment {
+	/*public static class PlaceholderFragment extends Fragment {
 
 		public PlaceholderFragment() {
 		}
@@ -57,5 +57,5 @@ public class HomeActivity extends ActionBarActivity {
 					false);
 			return rootView;
 		}
-	}
+	}*/
 }
--- Android/app/src/main/java/fr/exia/puydufou/MenuActivity.java
+++ Android/app/src/main/java/fr/exia/puydufou/MenuActivity.java
@@ -12,16 +12,13 @@ public class MenuActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_home);
-    final Button button=(Button) findViewById(R.id.button) button.setOnClickListener(new View.OnClickListener() {
+    final Button button=(Button) findViewById(R.id.button) ;
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                @Override
-                public void onClick(View) {
-                        ;
-                }
+                        int i;
             }
-        }
+        });
     }
 
 
