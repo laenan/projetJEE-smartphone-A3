afa19de827d067862a3bad8dec0b6fa8092c593c
sasvina <asvina.soundiram@viacesi.fr>
2015-06-17 15:00:39 +0200

    commit before pull

--- Android/app/src/main/java/fr/exia/puydufou/MenuActivity.java
+++ Android/app/src/main/java/fr/exia/puydufou/MenuActivity.java
@@ -1,6 +1,7 @@
 package fr.exia.puydufou;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,14 +13,15 @@ public class MenuActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_home);
-    final Button button=(Button) findViewById(R.id.button) button.setOnClickListener(new View.OnClickListener() {
+
+    final Button button=(Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                @Override
-                public void onClick(View) {
-                        ;
-                }
+                Intent intent = new Intent(MenuActivity.this, LoginDisplayActivity.class);
+                startActivity(intent);
+
             }
         }
     }
@@ -27,3 +29,4 @@ public class MenuActivity extends Activity {
 
 
 }
+
