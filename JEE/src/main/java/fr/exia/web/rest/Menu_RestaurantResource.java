7e7028fa4803142b6bc5c23e83a4a17e9d113bce
laenan <adrien.appelgren@viacesi.fr>
2015-06-18 17:23:09 +0200

    mend

--- JEE/src/main/java/fr/exia/web/rest/Menu_RestaurantResource.java
+++ JEE/src/main/java/fr/exia/web/rest/Menu_RestaurantResource.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
